cb891f3c8987a21582f520ee98df09da38e6cfa4
Emil Yay <keyt.isac@yandex.ru>
2025-08-28 05:37:34 +0300

    fix(migrations): added extension and index, edit timestamp to timestamptz (branch: user)

--- migrations/20250822024513_user_init.sql
+++ migrations/20250822024513_user_init.sql
@@ -1,21 +1,24 @@
 -- +goose Up
 -- +goose StatementBegin
 
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE users (
     id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email         TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    created_at    TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE refresh_tokens (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id     UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    token       TEXT NOT NULL UNIQUE,
-    expires_at  TIMESTAMP NOT NULL,
-    revoked     BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+    token_hash  TEXT NOT NULL,
+    expires_at  TIMESTAMPTZ NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
+
+CREATE INDEX idx_refresh_tokens_hash ON refresh_tokens(token_hash);
 
 -- +goose StatementEnd
 
