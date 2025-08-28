d522ab611798de0e9e97f6c3257de4fb58f6e868
Emil Yay <keyt.isac@yandex.ru>
2025-08-29 01:51:56 +0300

    feat(domain): add struct tags for db mapping (branch: user)

--- internal/user/domain/user.go
+++ internal/user/domain/user.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type User struct {
-	ID           string
-	Email        string
-	PasswordHash string
-	CreatedAt    time.Time
+	ID           string    `db:"id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
 }
