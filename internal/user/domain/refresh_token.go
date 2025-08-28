599e191a106fe10f57a67107fc5b2156994e8030
Emil Yay <keyt.isac@yandex.ru>
2025-08-29 01:52:45 +0300

    feat(domain): add refresh token struct (branch: user)

---
+++ internal/user/domain/refresh_token.go
@@ -0,0 +1,11 @@
+package domain
+
+import "time"
+
+type RefreshToken struct {
+	ID        string    `db:"id"`
+	UserID    string    `db:"user_id"`
+	TokenHash string    `db:"token_hash"`
+	ExpiresAt time.Time `db:"expires_at"`
+	CreatedAt time.Time `db:"created_at"`
+}
