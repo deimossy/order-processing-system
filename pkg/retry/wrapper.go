b829de119ec33a73c3f33e3beb3cba244e98bb9d
Emil Yay <keyt.isac@yandex.ru>
2025-08-29 05:15:54 +0300

    feat(pkg): add retry wrapper for i/o operations (branch: user)

---
+++ pkg/retry/wrapper.go
@@ -0,0 +1,41 @@
+package retry
+
+import (
+	"context"
+	"errors"
+	"math"
+	"math/rand"
+	"time"
+)
+
+var ErrMaxRetriesAttemptsExceeded = errors.New("max retries attempts exceeded")
+
+func retry(ctx context.Context, fn func() error, maxRetries int, backoff time.Duration) error {
+	var err error
+
+	for attempt := 0; attempt < maxRetries; attempt++ {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		err = fn()
+		if err == nil {
+			return nil
+		}
+
+		if attempt == maxRetries-1 {
+			return err
+		}
+
+		backoff = backoff * time.Duration(math.Pow(2, float64(attempt)))
+		jitter := time.Duration(rand.Int63n(int64(backoff)))
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(backoff + jitter):
+		}
+	}
+
+	return ErrMaxRetriesAttemptsExceeded
+}
