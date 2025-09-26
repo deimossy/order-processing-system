8c62a0c85aa323c54f39464c68230dd63de9e2e5
emilyay <keyt.isac@yandex.ru>
2025-09-26 04:03:46 +0000

    feat(errors): move max attempts err from retry to errors module (branch: user)

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -3,8 +3,8 @@ package errors
 import "errors"
 
 var (
-	ErrNotFound            = errors.New("not found")
-	ErrAlreadyExists       = errors.New("already exists")
-	ErrValidation          = errors.New("validation error")
-	ErrCodeUniqueViolation = "23505"
+	ErrNotFound                   = errors.New("not found")
+	ErrAlreadyExists              = errors.New("already exists")
+	ErrValidation                 = errors.New("validation error")
+	ErrMaxRetriesAttemptsExceeded = errors.New("max retries attempts exceeded")
 )
