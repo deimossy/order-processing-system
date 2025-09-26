4e4c4b47fb32cd209050122ed7a919bd837855df
emilyay <keyt.isac@yandex.ru>
2025-09-26 11:58:33 +0000

    feat(jwt): add load priv key and generate access token (branch: user)

--- internal/user/auth/jwt.go
+++ internal/user/auth/jwt.go
@@ -1,1 +1,45 @@
 package auth
+
+import (
+	"crypto/rsa"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	priv, err := jwt.ParseRSAPrivateKeyFromPEM(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+func GenerateAccessToken(userID, email string, privKey *rsa.PrivateKey, ttl time.Duration) (string, time.Time, error) {
+	exp := time.Now().UTC().Add(ttl)
+
+	claims := &jwt.MapClaims{
+		"sub":   userID,
+		"exp":   exp,
+		"iat":   time.Now().UTC(),
+		"jti":   uuid.New().String(),
+		"email": email,
+	}
+
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodPS256, claims)
+
+	signedToken, err := rawToken.SignedString(privKey)
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	return signedToken, exp, nil
+}
