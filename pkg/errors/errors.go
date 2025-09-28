054c42b66ae2e6053bf449787a99572d03a07f24
emilyay <keyt.isac@yandex.ru>
2025-09-28 03:34:30 +0000

    feat(errs): add new errors for tokens (branch: user)

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -6,5 +6,7 @@ var (
 	ErrNotFound                   = errors.New("not found")
 	ErrAlreadyExists              = errors.New("already exists")
 	ErrValidation                 = errors.New("validation error")
+	ErrTokenExpired               = errors.New("token has been expires")
+	ErrTokenInvalid               = errors.New("token is invalid")
 	ErrMaxRetriesAttemptsExceeded = errors.New("max retries attempts exceeded")
 )
