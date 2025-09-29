4f758137449f3a840e01de28815096c6634be25e
Emil Yay <keyt.isac@yandex.ru>
2025-09-29 03:32:20 +0300

    feat(errs): add new service errors (branch: user)

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -9,4 +9,6 @@ var (
 	ErrTokenExpired               = errors.New("token has been expires")
 	ErrTokenInvalid               = errors.New("token is invalid")
 	ErrMaxRetriesAttemptsExceeded = errors.New("max retries attempts exceeded")
+	ErrUserNotFound               = errors.New("user not found")
+	ErrUserAlreadyExists          = errors.New("user already exists")
 )
