5c424f225725a774e6257fe59175a5e7690458ea
Emil Yay <keyt.isac@yandex.ru>
2025-10-02 11:52:20 +0300

    feat(service): add register method and interfaces to use repos and unit of work (branch: user)

--- internal/user/usecase/user_service.go
+++ internal/user/usecase/user_service.go
@@ -2,11 +2,20 @@ package usecase
 
 import (
 	"context"
-	"log/slog"
-
+	"crypto/rsa"
+	"errors"
+	"github.com/deimossy/order-processing-system/internal/user/auth"
 	"github.com/deimossy/order-processing-system/internal/user/config"
 	"github.com/deimossy/order-processing-system/internal/user/domain"
+	errs "github.com/deimossy/order-processing-system/pkg/errors"
+	"time"
 )
+
+type RefreshTokenRepo interface {
+	Save(ctx context.Context, token *domain.RefreshToken) error
+	GetByTokenHash(ctx context.Context, token string) (*domain.RefreshToken, error)
+	RevokeByTokenHash(ctx context.Context, token string) error
+}
 
 type UserRepo interface {
 	Save(ctx context.Context, user *domain.User) error
@@ -15,18 +24,106 @@ type UserRepo interface {
 	DeleteByID(ctx context.Context, id string) error
 }
 
-type UserService struct {
-	logger           *slog.Logger
-	cfg              config.Config
-	userRepo         UserRepo
-	refreshTokenRepo RefreshTokenRepo
+type Repositories interface {
+	UserRepo() UserRepo
+	RefreshTokenRepo() RefreshTokenRepo
 }
 
-func NewUserService(logger *slog.Logger, cfg config.Config, userRepo UserRepo, refreshTokenRepo RefreshTokenRepo) *UserService {
+type UnitOfWork interface {
+	WithTx(ctx context.Context, fn func(ctx context.Context, repos Repositories) error) error
+	WithoutTx(ctx context.Context, fn func(ctx context.Context, repos Repositories) error) error
+}
+
+type UserService struct {
+	cfg        config.Config
+	uow        UnitOfWork
+	privateKey *rsa.PrivateKey
+}
+
+func NewUserService(cfg config.Config, uow UnitOfWork, key *rsa.PrivateKey) *UserService {
 	return &UserService{
-		logger:           logger,
-		cfg:              cfg,
-		userRepo:         userRepo,
-		refreshTokenRepo: refreshTokenRepo,
+		cfg:        cfg,
+		uow:        uow,
+		privateKey: key,
 	}
+}
+
+func (s *UserService) Register(ctx context.Context, email, password string) (*domain.TokenPair, error) {
+	var (
+		user  *domain.User
+		token *domain.TokenPair
+	)
+
+	err := s.uow.WithTx(ctx, func(ctx context.Context, repos Repositories) error {
+		// user check
+		_, err := repos.UserRepo().GetByEmail(ctx, email)
+		if err == nil {
+			return errs.ErrUserAlreadyExists
+		}
+		// infra err
+		if !errors.Is(err, errs.ErrNotFound) {
+			return err
+		}
+
+		// hashing pass
+		hashedPassword, err := auth.HashPassword(password, s.cfg.BcryptCost)
+		if err != nil {
+			return err
+		}
+
+		// user struct
+		user = &domain.User{
+			Email:        email,
+			PasswordHash: hashedPassword,
+			CreatedAt:    time.Now(),
+		}
+
+		// saving user
+		err = repos.UserRepo().Save(ctx, user)
+		if err != nil {
+			if errors.Is(err, errs.ErrAlreadyExists) {
+				return errs.ErrUserAlreadyExists
+			}
+			return err
+		}
+
+		// generate refresh token
+		refreshToken, plain, err := auth.GenerateRefreshToken(user.ID, s.cfg.RefreshTokenTTL)
+		if err != nil {
+			return err
+		}
+
+		token = &domain.TokenPair{
+			RefreshToken: plain,
+		}
+
+		return repos.RefreshTokenRepo().Save(ctx, refreshToken)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// generate access token
+	accessToken, exp, err := auth.GenerateAccessToken(user.ID, user.Email, s.privateKey, s.cfg.AccessTokenTTL)
+	if err != nil {
+		return nil, err
+	}
+
+	token.UserID = user.ID
+	token.AccessToken = accessToken
+	token.ExpiresAt = exp
+
+	return token, nil
+}
+
+func (s *UserService) Login(ctx context.Context, email, password string) (*domain.TokenPair, error) {
+	return nil, nil
+}
+
+func (s *UserService) Logout(ctx context.Context, token string) error {
+	return nil
+}
+
+func (s *UserService) RefreshToken(ctx context.Context, token string) (*domain.TokenPair, error) {
+	return nil, nil
 }
