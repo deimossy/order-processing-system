9da1e1c79042ff575c2bcfcb2d3741928b1fc3c7
Emil Yay <keyt.isac@yandex.ru>
2025-10-02 11:30:54 +0300

    feat(retry): add shouldRetry func in params for single queries and txs, add new violation codes (branch: user)

--- pkg/retry/wrapper.go
+++ pkg/retry/wrapper.go
@@ -2,20 +2,42 @@ package retry
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgconn"
 	"math/rand"
 	"time"
 
 	errs "github.com/deimossy/order-processing-system/pkg/errors"
 )
 
-func Do(ctx context.Context, maxRetries int, backoff, maxBackoff time.Duration, fn func() error) error {
+var (
+	CodeAdminShutdownViolation      = "57P01"
+	CodeCrashShutdownViolation      = "57P02"
+	CodeCannotConnectViolation      = "57P03"
+	CodeSerializationViolation      = "40001"
+	CodeDeadlockViolation           = "40P01"
+	CodeTooManyConnectionsViolation = "53300"
+	CodeQueryCanceledViolation      = "57014"
+)
+
+func Do(
+	ctx context.Context,
+	maxRetries int,
+	backoff, maxBackoff time.Duration,
+	fn func() error,
+	shouldRetry func(error) bool,
+) error {
 	var err error
 
 	for attempt := 0; attempt < maxRetries; attempt++ {
 		err = fn()
 		if err == nil {
 			return nil
+		}
+
+		if !shouldRetry(err) {
+			return err
 		}
 
 		if attempt == maxRetries-1 {
@@ -34,4 +56,53 @@ func Do(ctx context.Context, maxRetries int, backoff, maxBackoff time.Duration, 
 	}
 
 	return fmt.Errorf("%w: last error: %v", errs.ErrMaxRetriesAttemptsExceeded, err)
+}
+
+func ShouldRetryTx(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case CodeSerializationViolation,
+			CodeDeadlockViolation:
+			return true
+		}
+		return false
+	}
+
+	if errors.Is(err, context.DeadlineExceeded) ||
+		errors.Is(err, context.Canceled) {
+		return true
+	}
+
+	return false
+}
+
+func ShouldRetryQuery(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case CodeAdminShutdownViolation,
+			CodeCrashShutdownViolation,
+			CodeCannotConnectViolation,
+			CodeTooManyConnectionsViolation,
+			CodeQueryCanceledViolation:
+			return true
+		}
+		return false
+	}
+
+	if errors.Is(err, context.DeadlineExceeded) ||
+		errors.Is(err, context.Canceled) {
+		return true
+	}
+
+	return false
 }
