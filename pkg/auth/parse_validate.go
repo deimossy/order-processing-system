93b1bbacd86263db4da2a9df86d124b2c355c7ac
Emil Yay <keyt.isac@yandex.ru>
2025-10-03 16:02:11 +0300

    feat(errs): add new errors and rename olds (branch: user)

--- pkg/auth/parse_validate.go
+++ pkg/auth/parse_validate.go
@@ -15,23 +15,23 @@ func ParseAndValidateAccessToken(token string, pubKey *rsa.PublicKey) (*domain.A
 
 	parsed, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, errs.ErrTokenInvalid
+			return nil, errs.ErrAccessTokenInvalid
 		}
 		return pubKey, nil
 	}, jwt.WithLeeway(2*time.Minute))
 	if err != nil {
 		if errors.Is(err, jwt.ErrTokenExpired) {
-			return nil, errs.ErrTokenExpired
+			return nil, errs.ErrAccessTokenExpired
 		}
 		return nil, err
 	}
 
 	if !parsed.Valid {
-		return nil, errs.ErrTokenInvalid
+		return nil, errs.ErrAccessTokenInvalid
 	}
 
 	if !jwtFieldsValid(claims) {
-		return nil, errs.ErrTokenInvalid
+		return nil, errs.ErrAccessTokenInvalid
 	}
 
 	return claims, nil
--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -3,12 +3,27 @@ package errors
 import "errors"
 
 var (
-	ErrNotFound                   = errors.New("not found")
-	ErrAlreadyExists              = errors.New("already exists")
-	ErrValidation                 = errors.New("validation error")
-	ErrTokenExpired               = errors.New("token has been expires")
-	ErrTokenInvalid               = errors.New("token is invalid")
+	// repo errs
+	ErrNotFound      = errors.New("not found")
+	ErrAlreadyExists = errors.New("already exists")
+	ErrValidation    = errors.New("validation error")
+
+	// access token errs
+	ErrAccessTokenExpired = errors.New("access token has been expires")
+	ErrAccessTokenInvalid = errors.New("access token is invalid")
+
+	// refresh token errs
+	ErrRefreshTokenNotFound = errors.New("refresh token not found")
+	ErrRefreshTokenExpired  = errors.New("refresh token has expired")
+	ErrRefreshTokenRevoked  = errors.New("refresh token has been revoked")
+
+	// user errs
+	ErrUserNotFound      = errors.New("user not found")
+	ErrUserAlreadyExists = errors.New("user already exists")
+
+	// retry err
 	ErrMaxRetriesAttemptsExceeded = errors.New("max retries attempts exceeded")
-	ErrUserNotFound               = errors.New("user not found")
-	ErrUserAlreadyExists          = errors.New("user already exists")
+
+	// pswd err
+	ErrPasswordMismatch = errors.New("password mismatch")
 )
