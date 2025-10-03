f417ec794c97280eb5e32191c4a2c5e371385bc7
Emil Yay <keyt.isac@yandex.ru>
2025-10-03 15:58:45 +0300

    fix(domain/ref_token): edit value field to pointer (branch: user)

--- internal/user/domain/refresh_token.go
+++ internal/user/domain/refresh_token.go
@@ -3,10 +3,10 @@ package domain
 import "time"
 
 type RefreshToken struct {
-	ID        string    `db:"id"`
-	UserID    string    `db:"user_id"`
-	TokenHash string    `db:"token_hash"`
-	ExpiresAt time.Time `db:"expires_at"`
-	CreatedAt time.Time `db:"created_at"`
-	RevokedAt time.Time `db:"revoked_at"`
+	ID        string     `db:"id"`
+	UserID    string     `db:"user_id"`
+	TokenHash string     `db:"token_hash"`
+	ExpiresAt time.Time  `db:"expires_at"`
+	CreatedAt time.Time  `db:"created_at"`
+	RevokedAt *time.Time `db:"revoked_at"`
 }
