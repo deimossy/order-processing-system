517600caedabd7e3566367e08a879f1f25e571a9
Emil Yay <keyt.isac@yandex.ru>
2025-10-30 05:17:49 +0300

    feat(handler): implement grpc method except GetProfile (branch: user)

--- internal/user/controller/grpc/handler.go
+++ internal/user/controller/grpc/handler.go
@@ -2,16 +2,18 @@ package grpc
 
 import (
 	"context"
+	"errors"
 	"github.com/deimossy/order-processing-system/internal/user/usecase"
+	errs "github.com/deimossy/order-processing-system/pkg/errors"
 	user_v1 "github.com/deimossy/order-processing-system/protobuf/user/v1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Controller struct {
 	user_v1.UnimplementedUserServiceServer
-
 	svc *usecase.UserService
 }
 
@@ -26,7 +28,19 @@ func (c *Controller) Register(ctx context.Context, req *user_v1.RegisterRequest)
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	return nil, status.Errorf(codes.OK, "method: Register not implemented")
+	domainTP, err := c.svc.Register(ctx, req.Email, req.Password)
+	if err != nil {
+		return nil, writeGRPCStatus(err)
+	}
+
+	TP := &user_v1.TokenPair{
+		AccessToken:          domainTP.AccessToken,
+		RefreshToken:         domainTP.RefreshToken,
+		AccessTokenExpiresAt: timestamppb.New(domainTP.ExpiresAt),
+		UserId:               domainTP.UserID,
+	}
+
+	return TP, status.Error(codes.OK, "user registered")
 }
 
 func (c *Controller) Login(ctx context.Context, req *user_v1.LoginRequest) (*user_v1.TokenPair, error) {
@@ -34,7 +48,19 @@ func (c *Controller) Login(ctx context.Context, req *user_v1.LoginRequest) (*use
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	return nil, status.Errorf(codes.OK, "method: Login not implemented")
+	domainTP, err := c.svc.Login(ctx, req.Email, req.Password)
+	if err != nil {
+		return nil, writeGRPCStatus(err)
+	}
+
+	TP := &user_v1.TokenPair{
+		AccessToken:          domainTP.AccessToken,
+		RefreshToken:         domainTP.RefreshToken,
+		AccessTokenExpiresAt: timestamppb.New(domainTP.ExpiresAt),
+		UserId:               domainTP.UserID,
+	}
+
+	return TP, status.Error(codes.OK, "user logged in")
 }
 
 func (c *Controller) Refresh(ctx context.Context, req *user_v1.RefreshRequest) (*user_v1.TokenPair, error) {
@@ -42,7 +68,19 @@ func (c *Controller) Refresh(ctx context.Context, req *user_v1.RefreshRequest) (
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	return nil, status.Errorf(codes.OK, "method: Refresh not implemented")
+	domainTP, err := c.svc.Refresh(ctx, req.RefreshToken)
+	if err != nil {
+		return nil, writeGRPCStatus(err)
+	}
+
+	TP := &user_v1.TokenPair{
+		AccessToken:          domainTP.AccessToken,
+		RefreshToken:         domainTP.RefreshToken,
+		AccessTokenExpiresAt: timestamppb.New(domainTP.ExpiresAt),
+		UserId:               domainTP.UserID,
+	}
+
+	return TP, status.Error(codes.OK, "user refreshed")
 }
 
 func (c *Controller) Logout(ctx context.Context, req *user_v1.LogoutRequest) (*emptypb.Empty, error) {
@@ -50,7 +88,12 @@ func (c *Controller) Logout(ctx context.Context, req *user_v1.LogoutRequest) (*e
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	return nil, status.Errorf(codes.OK, "method: Logout not implemented")
+	err := c.svc.Logout(ctx, req.RefreshToken)
+	if err != nil {
+		return nil, writeGRPCStatus(err)
+	}
+
+	return &emptypb.Empty{}, status.Error(codes.OK, "user logout")
 }
 
 func (c *Controller) GetProfile(ctx context.Context, req *user_v1.GetProfileRequest) (*user_v1.UserProfile, error) {
@@ -59,4 +102,29 @@ func (c *Controller) GetProfile(ctx context.Context, req *user_v1.GetProfileRequ
 	}
 
 	return nil, status.Errorf(codes.OK, "method: GetProfile not implemented")
+}
+
+func writeGRPCStatus(err error) error {
+	switch {
+	case errors.Is(err, errs.ErrUserNotFound), errors.Is(err, errs.ErrRefreshTokenNotFound):
+		return status.Error(codes.NotFound, err.Error())
+
+	case errors.Is(err, errs.ErrUserAlreadyExists):
+		return status.Error(codes.AlreadyExists, err.Error())
+
+	case errors.Is(err, errs.ErrAccessTokenInvalid), errors.Is(err, errs.ErrPasswordMismatch):
+		return status.Error(codes.InvalidArgument, err.Error())
+
+	case errors.Is(err, errs.ErrAccessTokenExpired):
+		return status.Error(codes.FailedPrecondition, err.Error())
+
+	case errors.Is(err, errs.ErrRefreshTokenExpired), errors.Is(err, errs.ErrRefreshTokenRevoked):
+		return status.Error(codes.Unauthenticated, err.Error())
+
+	case errors.Is(err, errs.ErrMaxRetriesAttemptsExceeded):
+		return status.Error(codes.Unavailable, err.Error())
+
+	default:
+		return status.Error(codes.Internal, err.Error())
+	}
 }
