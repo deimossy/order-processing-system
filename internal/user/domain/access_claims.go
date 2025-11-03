18356f604bb50acb4e69be04b53bd135b475844b
Emil Yay <keyt.isac@yandex.ru>
2025-11-03 19:15:42 +0300

    chore(deps): add new libs (branch: user)

--- internal/user/domain/access_claims.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "github.com/golang-jwt/jwt/v5"
-
-// обертка над дефолтными клеймсами
-type AccessTokenClaims struct {
-	Email string `json:"email"`
-	jwt.RegisteredClaims
-}
